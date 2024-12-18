5665b5dfd1d3b170974828d847a814653d12bc21
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2024-12-18 21:06:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://leading-tomasina-doveloperboy-08e5a1b5.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
